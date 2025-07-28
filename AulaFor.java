d935aae49048053da14788e813178c86f24a19d7
Henrique Silva <henriiisilva2007@gmail.com>
2025-07-27 23:20:06 -0300

    Add files via upload
    Um pouco de como é a funcionalidade do For

---
+++ AulaFor.java
@@ -0,0 +1,11 @@
+public class AulaFor {
+    public static void main(String[] args) {
+
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+
+            System.out.println("Contando os carneirinhos kkkkk: " + carneirinhos);
+
+        }
+        System.out.println("Fim da contagem de carneiros kkkkkkkk");
+    }
+}
---
+++ ForArray.java
@@ -0,0 +1,10 @@
+public class ForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Henrique", "Pedro", "Matheus", "Leonardo", "Marcos"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no inde i= " + i + " é " + alunos[i]);
+
+        }
+    }
+}
